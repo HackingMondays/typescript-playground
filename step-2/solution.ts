26be114590293ce6dee98d1d3215e714be3a789e
Ludovic Lafole <ludovic.lafole@gmail.com>
2015-10-26 20:05:45 +0100

    fix solution bis

--- step-2/solution.ts
+++ step-2/solution.ts
@@ -1,14 +1,10 @@
 interface Fruits {
     price():Number;
-    is(name: Stirng): Boolean;
 }
 
 class Pommes implements Fruits {
     public price():Number {
         return 100
-    }
-    public is(name: String): Boolean {
-        return name == "Pommes"
     }
 }
 
@@ -16,17 +12,11 @@ class Cerises implements Fruits {
     public price():Number {
         return 75
     }
-    public is(name: String): Boolean {
-        return name == "Cerises"
-    }
 }
 
 class Bananes implements Fruits {
     public price():Number {
         return 150
-    }
-    public is(name: String): Boolean {
-        return name == "Bananes"
     }
 }
 
@@ -34,7 +24,7 @@ class CaisseEnregistreuse {
     public calculTotal(entrees:Array<Fruits>): Number {
         var total = entrees.map(fruit => fruit.price())
             .reduce((acc, price) => acc + price, 0);
-        var reduction = Math.floor(entrees.filter(fruit => fruit.is("Cerises")).length/2) * 20
+        var reduction = Math.floor(entrees.filter(fruit => fruit instanceof Cerises).length/2) * 20
 
         return total - reduction;
     }
